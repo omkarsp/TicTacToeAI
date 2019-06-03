1c3efe80c372a9c2faa87c4294e2ab515f024162
omkarsp <omkarposhatwar12345@gmail.com>
2019-06-04 01:04:20 +0530

    Working prototype
    Submission.

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -9,10 +9,12 @@ public class Gameplay : MonoBehaviour {
     public Cell turn;//for multiplayer
     public string CircleTag = "circle";
     public string CrossTag = "cross";
-    public int TurnCount = 0;
-    public int CircleTurn = 0;
-    public Text FinishText;
-    public string WinnerTag = null;
+    public string DrawTag = "Draw";
+        public Text FinishText;
+
+    int TurnCount = 0;
+    int CircleTurn = 0;
+    string WinnerTag = null;
 
     public void SpawnCross(GameObject obj)
     {
@@ -51,63 +53,90 @@ public class Gameplay : MonoBehaviour {
 
         if ((Cells[0].tag == Cells[1].tag) && (Cells[1].tag == Cells[2].tag))
         {
+            WinnerTag = Cells[1].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[3].tag == Cells[4].tag) && (Cells[4].tag == Cells[5].tag))
         {
+            WinnerTag = Cells[3].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[6].tag == Cells[7].tag) && (Cells[7].tag == Cells[8].tag))
         {
+            WinnerTag = Cells[6].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[0].tag == Cells[3].tag) && (Cells[3].tag == Cells[6].tag))
         {
+            WinnerTag = Cells[0].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[1].tag == Cells[4].tag) && (Cells[4].tag == Cells[7].tag))
         {
+            WinnerTag = Cells[1].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
-    
+
         else if ((Cells[2].tag == Cells[5].tag) && (Cells[5].tag == Cells[8].tag))
         {
+            WinnerTag = Cells[2].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[0].tag == Cells[4].tag) && (Cells[4].tag == Cells[8].tag))
         {
+            WinnerTag = Cells[0].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if ((Cells[2].tag == Cells[4].tag) && (Cells[4].tag == Cells[6].tag))
         {
+            WinnerTag = Cells[2].tag;
             FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
 
         else if (TurnCount == 9)
         {
+            WinnerTag = DrawTag;
+            FindObjectOfType<ShowMessage>().ActivateUI();
+            FinalText();
             //FindObjectOfType<SceneLoading>().LoadLastScene();
         }
     }
 
     public void FinalText()
     {
-        if (Winnertag == "cross")
+        if (WinnerTag == CrossTag)
         {
-
+            FinishText.text = "You Win!";
+        }
+        else if(WinnerTag == CircleTag)
+        {
+            FinishText.text = "Bot Wins!";
+        }
+        else if(WinnerTag == DrawTag)
+        {
+            FinishText.text = "Draw!";
         }
     }
 
--- Assets/Scripts/ShowMessage.cs
+++ Assets/Scripts/ShowMessage.cs
@@ -4,25 +4,29 @@ using UnityEngine.UI;
 public class ShowMessage : MonoBehaviour {
 
 
-    public Text GameOverText = null;
-    public Text WinText = null;
+    //public Text GameOverText = null;
+    //public Text WinText = null;
     public GameObject GameOverUI;
+    public GameObject InGameUI;
+    public GameObject EmptyBoxes;
 
 
-    public void GameOverMessage()
-    {
-        GameOverText.text = "GAME OVER!";
-        WinText.enabled = false;
-    }
+    //public void GameOverMessage()
+    //{
+    //    GameOverText.text = "GAME OVER!";
+    //    WinText.enabled = false;
+    //}
 
-    public void WinMessage(GameObject Winner)
-    {
-        WinText.text = Winner.tag + " WON!";
-        GameOverText.enabled = false;
-    }
+    //public void WinMessage(GameObject Winner)
+    //{
+    //    WinText.text = Winner.tag + " WON!";
+    //    GameOverText.enabled = false;
+    //}
 
     public void ActivateUI()
     {
         GameOverUI.SetActive(true);
+        InGameUI.SetActive(false);
+        EmptyBoxes.SetActive(false);
     }
 }
